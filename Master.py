93cc4acdc6aca3d16f3ce677ed287994a90a35a7
Ashwin Balasubramanian <balathinnappan@gmail.com>
2018-03-14 13:29:59 -0700

    stablized master

--- Master.py
+++ Master.py
@@ -2,4 +2,4 @@ import account
 
 if __name__ == '__main__':
     Address1 = 'GBCKQLHFUZF36FIJUKUW37YFQG5O5MXT3YB2M7ZGVBORLUZMPIITWHZI'
-    account.Details(Address1).print_details()
+    account.Details(Address1).get()
